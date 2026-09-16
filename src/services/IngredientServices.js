c50a2c4487da27e4dfb5c1024e2a9f3fa5745883
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-16 14:41:33 +0200

    feat(frontend services): add IngredientServices.js

---
+++ src/services/IngredientServices.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:5253/api/',
+
+})
+
+const getAllIngredients = async () => {
+    const response = await api.get('ingredients')
+    return response.data
+}
+
+const getIngredientById = async id => {
+    const response = await api.get(`ingredients/${id}`)
+    return response.data
+}
+
+const createIngredient = async ingredient => {
+    const response = await api.post('ingredients', ingredient)
+    return response.data
+}
+
+const updateIngredient = async (id, newValue, attribute) => {
+    const response = await api.put(`ingredients/${id}`, { NewValue: newValue, Attribute: attribute })
+    return response.data
+}
+
+const deleteIngredient = async id => {
+    const response = await api.delete(`ingredients/${id}`)
+    return response.data
+}
+
+export { getAllIngredients, getIngredientById, createIngredient, updateIngredient, deleteIngredient };
