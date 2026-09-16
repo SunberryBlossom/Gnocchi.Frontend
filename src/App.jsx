51c47e6f6bdd10908c66912e05fd7c32bdac0295
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-16 16:42:09 +0200

    test(App): try out context and provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import { DishProvider } from './context/DishContext'
+import DishList from './components/DishList'
+import DishCreate from './components/DishCreate'
+import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <header>
+        <h1>Welcome to the Gnocchi App</h1>
+      </header>
+      <DishProvider>
+        <DishList />
+        <DishCreate />
+      </DishProvider>
     </>
   )
 }
