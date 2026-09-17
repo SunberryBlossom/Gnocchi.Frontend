e8e84eae1cde0d6569fac1c6d4fd5bfebf9db624
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-17 15:03:09 +0200

    feat(main): add react router browser-router

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router >
+      <App />
+    </Router>
   </StrictMode>,
 )
