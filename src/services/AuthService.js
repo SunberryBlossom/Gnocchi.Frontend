938aadd542ff70bdb0002d329edfe705b4d8bb1c
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-17 17:13:39 +0200

    feat: add signup function

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -9,6 +9,10 @@ async function Signin(email, password) {
         await api.post('login?useCookies=true', { email, password,})
 }
 
+async function Signup(email, password) {
+    await api.post('register', { email, password })
+}
+
 async function CheckAuth() {
     try {
         await api.get('manage/info')
@@ -19,4 +23,4 @@ async function CheckAuth() {
         }
     }
 }
-export { Signin, CheckAuth }
+export { Signin, CheckAuth, Signup }
