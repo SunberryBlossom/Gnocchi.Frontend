6f29ad00979cebea8cae9dfa18fea90519378e66
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-17 17:15:55 +0200

    add MUI CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { BrowserRouter as Router} from 'react-router-dom';
-
-import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router';
+import { CssBaseline } from '@mui/material';
+import { App } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router >
+      <CssBaseline />
       <App />
     </Router>
   </StrictMode>,
