e30af53618594552dc5deca46223da600694d481
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-17 17:11:23 +0200

    feat: add 404 page

--- src/pages/MissingPage.jsx
+++ src/pages/MissingPage.jsx
@@ -1,5 +1,14 @@
 
 
+function MissingPage() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  )
+}
+
 
 
 export { MissingPage}
