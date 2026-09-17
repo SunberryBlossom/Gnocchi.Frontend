3147538d5761ab1124a1cb44845f69c4aab13638
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-17 17:15:32 +0200

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import './App.css'
-import { useState } from 'react'
 import { Routes, Route } from 'react-router'
-import { LoginPage } from './pages/LoginPage.jsx'
+import { DishesPage } from './pages/DishesPage.jsx'
+import { AddLogPage } from './pages/AddLogPage.jsx'
+import { SigninPage } from './pages/SigninPage.jsx'
 import { SignupPage } from './pages/SignupPage.jsx'
 import { DashboardPage } from './pages/DashboardPage.jsx'
 import { MissingPage } from './pages/MissingPage.jsx'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
+import { ProtectedRoute } from './components/ProtectedRoute.jsx'
+import Layout from './components/Layout.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Routes>
-      <Route path='/' element={<LoginPage />} />
+      <Route path='/' element={<SigninPage />} />
       <Route path='/signup' element={<SignupPage />} />
-      <Route path='/dashboard' element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+
+      <Route
+        element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+        <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/add-log' element={<AddLogPage />} />
+        <Route path='/dishes' element={<DishesPage />} />
+      </Route>
       <Route path='*' element={<MissingPage />} />
     </Routes>
   )
 }
 
-export default App
+export { App }
