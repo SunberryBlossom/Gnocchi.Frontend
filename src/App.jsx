8df0702009996055fe9723c4b7b7a8260c83fb51
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-18 16:12:52 +0200

    chore: reformat App return jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,7 @@ function App() {
       <Route path='/' element={<SigninPage />} />
       <Route path='/signup' element={<SignupPage />} />
 
-      <Route
-        element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+      <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
         <Route path='/dashboard' element={<DashboardPage />} />
         <Route path='/add-log' element={<AddLogPage />} />
         <Route path='/dishes' element={<DishesPage />} />
